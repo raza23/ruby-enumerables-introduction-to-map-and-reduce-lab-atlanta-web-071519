6fe509a2f496369533108bcfcd798133cfea9d03
Raza Tahir Shareef <rtshareef@gmail.com>
2019-08-04 06:03:53 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -33,9 +33,9 @@ end
 
 def reduce_to_any_true(source_array)
   source_array.reduce do |x|
-    if x = true
+    if x == true
        x
-    elsif x = false
+    elsif x == false
        false
 
     end

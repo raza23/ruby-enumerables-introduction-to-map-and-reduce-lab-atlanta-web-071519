bfeea541a1b0237ae6dd09fc1e6e2c429fdf830a
Raza Tahir Shareef <rtshareef@gmail.com>
2019-08-04 04:42:37 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,39 @@
-# My Code here....
+require 'pry'
+def map_to_negativize(source_array)
+  source_array.map{|n| n * -1}
+end
+
+def map_to_no_change(source_array)
+  source_array.map{|n| n * 1}
+end
+
+def map_to_double(source_array)
+  source_array.map{|n| n*2}
+end
+
+def map_to_square(source_array)
+  source_array.map{|n| n*n}
+end
+
+def reduce_to_total(source_array,starting_point=0)
+  source_array.reduce(:+)
+
+
+end
+
+
+def reduce_to_total(source_array,starting_point = 0)
+  source_array.reduce(:+) + starting_point
+
+end
+
+def reduce_to_all_true(source_array)
+  source_array.reduce(true,:&)
+
+end
+
+def reduce_to_any_true(source_array)
+  source_array.map(|x| x = 'true')
+
+
+end
